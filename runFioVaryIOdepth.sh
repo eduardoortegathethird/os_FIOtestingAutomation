ae7e46548884f773cc91c10eb150a3f6461a02ac
eortega3 <eortega@sandiego.edu>
2020-05-10 13:45:53 -0700

    completed windows test

--- runFioVaryIOdepth.sh
+++ runFioVaryIOdepth.sh
@@ -1,5 +1,6 @@
-ioVaryDepth=(1 2 4 8)
+ioVaryDepth=(1 2 4 8 16 32 64 128 256)
 
-for io in "${ioVaryDepth[@]}"; do
-    echo "fio RR.ini.txt --iodepth=$io --output=testResult_$io.json+  --output-format=json+"
+for io in "${ioVaryDepth[@]}";
+do
+	fio RR.ini.txt --iodepth=$io --output=testResult_write_$io.json+  --output-format=json+
 done
